4ec76eb764d0dec7ca33b1ff5a00f187da4701ee
Naresh Prajapati <np749294@gmail.com>
2026-02-25 12:48:59 +0530

    initial commit

---
+++ core/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render,redirect
+from .forms import UserSignupForm , UserLoginForm
+from django.contrib.auth import authenticate,login
+
+# Create your views here.
+def userSignupView(request):
+    if request.method =="POST":
+      form = UserSignupForm(request.POST or None)
+      if form.is_valid():
+        form.save()
+        return redirect('login') #error
+      else:
+        return render(request,'core/signup.html',{'form':form})  
+    else:
+        form = UserSignupForm()
+        return render(request,'core/signup.html',{'form':form})
+    
+
+def userLoginView(request):
+  if request.method =="POST":
+    form = UserLoginForm(request.POST or None)
+    if form.is_valid():
+      print(form.cleaned_data)
+      email = form.cleaned_data['email']
+      password = form.cleaned_data['password']
+      user = authenticate(request,email=email,password=password) #it will check in database..
+      if user:
+        login(request,user)
+        if user.role == "admin":
+          return redirect("admin_dashboard")
+        elif user.role == "user":
+          return redirect("user_dashboard")       
+        elif user.role == "servicestaff":
+          return redirect("servicestaff_dashboard")
+    
+      else:
+        return redirect("login")
+  
+    else:
+      return render(request,'core/login.html',{'form':form})  
+    
+  else:
+    form = UserLoginForm()
+    return render(request,'core/login.html',{'form':form})
